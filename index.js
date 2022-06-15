9dd4be8ea1b237792808d8d450f92c801f7be7c3
MateusFigueiredo <figueiredo1497@gmail.com>
2022-06-15 18:02:48 -0300

    Search for month infos

--- index.js
+++ index.js
@@ -5,6 +5,21 @@ import { IsToday } from './IsToday.js';
 
 const port = process.env.PORT || 5000;
 
+function SearchMonthHoliday(month){
+    const holidays = Holidays();
+    const array = [];
+    for(let i = 0; i < holidays.length; i++){
+        let iMonth = holidays[i].date.split("/");
+        if(month === Number(iMonth[0])){
+            array.push(holidays[i]);
+        }
+    }
+
+    return (
+        array
+    );
+}
+
 const app = express();
 app.use(cors());
 
@@ -14,6 +29,12 @@ app.get('/holidays', (request, response) => {
 
 app.get('/is-today-holiday', (request, response) => {
     response.send(IsToday());
+});
+
+app.get('/holidays/:idMes', (request, response) => {
+    const id = Number(request.params.idMes);
+    const res = SearchMonthHoliday(id);
+    response.send(res);
 });
 
 app.listen(port);
